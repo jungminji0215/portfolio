1c5fcd2e54040ba390b9e56e4e3163bde4afb7c1
jungminji <wjdalswl0215@gmail.com>
2025-02-17 05:16:44 +0900

    main - 메인 소개 영역

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
+import Main from "./components/Main";
+
 export default function Home() {
   return (
     <>
-      <section>Main</section>
-      <section>About Me</section>
-      <section>Skills</section>
-      <section>Projects</section>
-      <section>Career</section>
-      <section>Contact Me</section>
+      <section className="w-11/12 max-w-3xl mx-auto h-screen flex items-center justify-center">
+        <Main />
+      </section>
+      <section className="bg-red-300">About Me</section>
+      <section className="bg-orange-300">Skills</section>
+      <section className="bg-gray-300">Projects</section>
+      <section className="bg-yellow-300">Career</section>
+      <section className="bg-blue-300">Contact Me</section>
     </>
   );
 }
