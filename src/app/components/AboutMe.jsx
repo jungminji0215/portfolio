461a6240ee5c797abd9655310d6024f92f16c2ce
jungminji <wjdalswl0215@gmail.com>
2025-02-23 13:45:15 +0900

    About Me 영역

--- src/app/components/AboutMe.jsx
+++ src/app/components/AboutMe.jsx
@@ -1,70 +1,72 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function AboutMe() {
   return (
+    // TODO scroll-mt-20 지우기
     <div className="bg-red-200 w-full max-w-3xl mx-auto flex flex-col items-center py-5 scroll-mt-20">
-      <h2 className="text-3xl">About Me</h2>
+      <h2 className="text-5xl font-bold mb-5">About Me</h2>
 
-      <div className="about-me max-container">
-        <section className="about-me__description">
-          <p className="about-me__description__message">
-            성장과 도전의 가치를 알고 <br />
-            배우고 부딪히며 꾸준히 발전하고있는 <br />
-            정민지입니다.
+      <div className="flex gap-10">
+        <section className="bg-yellow-300 flex items-center">
+          <p className="text-xl">
+            어쩌구 저쩌구 어쩌구 저쩌구 <br />
+            어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 <br />
+            정민지입니다!
           </p>
         </section>
 
-        <section className="about-me__info_items">
-          <div className="about-me__item">
-            <div className="about-me__item__title">
-              <i className="fa-solid fa-user about-me__item__title__icon"></i>
-              <p>이름</p>
-            </div>
-            <p className="about-me__item__content">정민지</p>
-          </div>
-
-          <div className="about-me__item">
-            <div className="about-me__item__title">
-              <i className="fa-regular fa-envelope about-me__item__title__icon"></i>
-              <p>이메일</p>
-            </div>
-            <p className="about-me__item__content">wjdalswl0215@gmail.com</p>
-          </div>
-
-          <div className="about-me__item">
-            <div className="about-me__item__title">
-              <i className="fa-regular fa-file about-me__item__title__icon"></i>
-              <p>이력서</p>
-            </div>
-            <a className="about-me__item__content"> 링크 </a>
-          </div>
-
-          <div className="about-me__item">
-            <div className="about-me__item__title">
-              <i className="fa-brands fa-github about-me__item__title__icon"></i>
-              <p>깃허브</p>
-            </div>
-            <a
+        <section className="bg-yellow-200 flex flex-col gap-5">
+          <div className="flex justify-center gap-10">
+            <Link
               href="https://github.com/jungminji0215"
               target="_blank"
-              className="about-me__item__content"
+              className="flex flex-col gap-1 items-center"
             >
-              https://github.com/jungminji0215
-            </a>
-          </div>
-
-          <div className="about-me__item">
-            <div className="about-me__item__title">
-              <i className="fa-solid fa-blog about-me__item__title__icon"></i>
-              <p>블로그</p>
-            </div>
-            <a
+              <Image
+                src={"/images/github.svg"}
+                width={50}
+                height={50}
+                alt="깃허브"
+              />
+              <p className="text-sm font-bold">깃허브</p>
+            </Link>
+            <Link
               href="https://jungminji0215.tistory.com/"
               target="_blank"
-              className="about-me__item__content"
+              className="flex flex-col gap-1 items-center"
             >
-              https://jungminji0215.tistory.com/
-            </a>
+              <Image
+                src={"/images/github.svg"}
+                width={50}
+                height={50}
+                alt="블로그"
+              />
+              <p className="text-sm font-bold">블로그</p>
+            </Link>
+          </div>
+
+          <div className="flex gap-5">
+            <div className="flex flex-col items-center gap-1">
+              <Image
+                src={"/images/github.svg"}
+                width={50}
+                height={50}
+                alt="학교"
+              />
+              <p className="text-sm">OO대학교</p>
+              <p className="text-sm">IT융합공학부</p>
+            </div>
+            <div className="flex flex-col items-center gap-1">
+              <Image
+                src={"/images/github.svg"}
+                width={50}
+                height={50}
+                alt="자격증"
+              />
+              <p className="text-sm">정보처리기사</p>
+            </div>
           </div>
         </section>
       </div>
