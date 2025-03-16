f36279b68c12ff16d45bd42a8289b67df38ab23d
jungminji <wjdalswl0215@gmail.com>
2025-03-16 15:25:32 +0900

    add open graph

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,24 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export const metadata = {
-  title: "정민지 포트폴리오",
+  title: "정민지 포트폴리오 사이트",
   description: "개발자 정민지의 포트폴리오 사이트",
+
+  openGraph: {
+    type: "website",
+    title: "정민지 포트폴리오 사이트",
+    description: "웹 개발자 정민지의 정민지 포트폴리오 사이트",
+    url: "https://jungminji.vercel.app/",
+    siteName: "정민지 포트폴리오 사이트",
+    images: [
+      {
+        url: "/jungminji.png",
+        width: 1200,
+        height: 630,
+        alt: "jungminji image",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
