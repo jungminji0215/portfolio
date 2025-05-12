942b94619508408f805a344a3019d9e691528a86
jungminji <wjdalswl0215@gmail.com>
2025-05-13 08:30:46 +0900

    [build] install webpack-bundle-analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, { isServer }) {
+    if (process.env.ANALYZE === 'true') {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          openAnalyzer: false,
+          reportFilename: isServer ? '../analyze/server.html' : './analyze/client.html',
+        }),
+      );
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
